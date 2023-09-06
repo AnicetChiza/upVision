f244cbd20129b0c6449b770f008437196a60b9b0
unknown <chizaanicet@gmail.com>
2023-09-06 15:36:58 +0000

    add to my repository

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,3 +1,12 @@
+const menu = document.querySelector('.menu-icons');
+const exit = document.querySelector('.menu-container');
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('menu-mobile');
+    exit.classList.toggle('menu-mobile');
+})
+
+
 const scroll = document.querySelector('.top');
 scroll.addEventListener('click', () =>{
     window.scrollTo({
